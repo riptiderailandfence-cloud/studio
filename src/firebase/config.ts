b6c96ce441d34c5fe9602fc24b1db20089b4e250
McCray Chappell <riptiderailandfence@gmail.com>
2026-03-02 23:51:38 +0000

    add these api keys to the .env file apiKey: "AIzaSyAYSFRehdE-KA59MLF9P7i

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-1859769021-3419f",
-  "appId": "1:163076726298:web:4fc075a02cd254b8e33ba3",
-  "apiKey": "AIzaSyAYSFRehdE-KA59MLF9P7ig52cr7MtNI6I",
-  "authDomain": "studio-1859769021-3419f.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "163076726298"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyAYSFRehdE-KA59MLF9P7ig52cr7MtNI6I",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "studio-1859769021-3419f.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "studio-1859769021-3419f",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "studio-1859769021-3419f.firebasestorage.app",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "163076726298",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:163076726298:web:4fc075a02cd254b8e33ba3"
 };

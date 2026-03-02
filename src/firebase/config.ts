230a1a222a24f80c3facace2718a7cb234657f32
McCray Chappell <riptiderailandfence@gmail.com>
2026-03-02 22:13:41 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1859769021-3419f",
+  "appId": "1:163076726298:web:4fc075a02cd254b8e33ba3",
+  "apiKey": "AIzaSyAYSFRehdE-KA59MLF9P7ig52cr7MtNI6I",
+  "authDomain": "studio-1859769021-3419f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "163076726298"
+};
